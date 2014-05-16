018b18bfbd358eace64a36cfd7bb4b950788708d
Alucea <aarons104@gmail.com>
2014-05-15 22:17:55 -0400

    removed background.jpg, removed background.png, added Domine Font, changed background, redesigned site layout, re styled site.

--- scripts.js
+++ scripts.js
@@ -1,53 +1,47 @@
 $(document).ready(function() {
 
-	function about_me() {
-		//when the return text is clicked on the page, the html is changed
-		$("#return").click(function() {
-			init();
-		})
-	}
-
-	function projects() {
+	function reinit() {
 
 		$("#return").click(function() {
 			init();
-		})
+		});
 	}
 
 	function init() {
 		//this stores the initial content for the page menu
-		var temp = "<h1 id='menu-header' class='border'>Main Menu</h1><ul><li class='border' id ='projects'>Projects</li><br><li><a href='./resources/resume.pdf' id='resume'>click here to view my résumé (PDF)</a></li><br><li class='border' id='about'> About Me</li></ul>"
+		var temp = "<h1 id='menu-header'>MAIN MENU</h1><ul><li id ='projects'>PROJECTS</li><br><li><a href='./resources/resume.pdf' id='resume'>RÉSUMÉ (PDF)</a></li><br><li id='about'> ABOUT ME</li></ul>"
 		//put content on page
 		$("#main-menu").html(temp);
 		//if about me text is clicked:
 		$("#about").click(function() {
-			//holds a description of myself
-			var foo = "My name is Aaron Sedlacek, I'm an Information Technology and Web Science Student at Rensselaer Polytechnic Institute. My main areas of focus are Information Security and Cryptography. I graduated from Chaminade High School in 2010 and I am a member of the International Fraternity of Phi Gamma Delta. My primary areas of expertise are Web Application Exploitation, Penetration Testing, and Social Engineering.";
-			//switches the text shown on the page with my description
-			$("#main-menu").html("<div id='description' class='border'><h1 id='menu-header' class='border'>About Me</h1>" + foo + "</div><div id='return' class='border'>Return to Main Menu</div>");
-			//about_me function calls this function again once the user decides to return to the original menu
-			about_me();
-		})
+
+			var desc = "My name is Aaron Sedlacek, I'm an Information Technology and Web Science Student at Rensselaer Polytechnic Institute. My main areas of focus are Information Security and Cryptography. I graduated from Chaminade High School in 2010 and I am a member of the International Fraternity of Phi Gamma Delta. My primary areas of expertise are Web Application Exploitation, Penetration Testing, and Social Engineering.";
+	
+			var foo = "<h1 id='menu-header'>ABOUT ME</h1><div id='desc'>" + desc + "</div>";
+
+			$("#main-menu").html("<div id='about-me'>" + foo + "</div><br/><div id='return'>RETURN TO MAIN MENU</div>");
+
+			reinit();
+		});
 
 		$("#projects").click(function() {
 
-			var foo = "<h1 id='menu-header' class='border'>Projects</h1><ul><li><a href='./hexxed/index.html'>Hexxed</a></li><br><li><a href='./budgetory/index.php'>Budgetory</a></li><br><li><a href='http://www.rpibookswap.com'>rpibookswap.com</a></li><br><li><a href='http://upost.herokuapp.com/'>uPost</a></li></ul>";
+			var foo = "<h1 id='menu-header'>PROJECTS</h1><ul><li><a href='./hexxed/index.html'>HEXXED</a></li><br><li><a href='./budgetory/index.php'>BUDGETORY</a></li><br><li><a href='http://www.rpibookswap.com'>RPIBOOKSWAP</a></li><br><li><a href='http://upost.herokuapp.com/'>uPost</a></li></ul>";
 
-			$("#main-menu").html("<div id='projects-list'>" + foo + "</div><div id='return' class='border'>Return to Main Menu</div>");
+			$("#main-menu").html("<div id='projects-list'>" + foo + "</div><div id='return'>RETURN TO MAIN MENU</div>");
 
-			projects();
+			reinit();
 
-		})
+		});
 	}
-	
 	//calls init function
 	init();
 	//makes main manu draggable
 	$(function() {
     	$( "#main-menu" ).draggable();
-  	})
+  	});
 	//makes my image draggable
 	$(function() {
     	$( "#my_image" ).draggable();
-  	})
+  	});
 });
